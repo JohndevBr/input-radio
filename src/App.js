b1eafdd0e01ce779d44358c70819558c1d12d1e4
João Cláudio <jca.oliveira91@gmail.com>
2023-02-16 01:37:11 -0300

    Finalizado projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BsInstagram, BsLinkedin } from "react-icons/bs"
 import './App.css';
 
 function App() {
+  const [inputChecked, setInputChecked] = useState("")
+
+  function handleChangeValue(event){
+    setInputChecked(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputDiv">
+        <label htmlFor="Linkedin"><BsLinkedin /></label>
+        <input 
+          type="radio" 
+          name="Linkedin" 
+          value="Linkedin"
+          checked={inputChecked === "Linkedin"}
+          onChange={handleChangeValue}
+        />
+        </div>
+      <div className="inputDiv">  
+        <label htmlFor="Instagram"><BsInstagram /></label>
+        <input 
+          type="radio" 
+          name="Instagram" 
+          value="Instagram"
+          checked={inputChecked === "Instagram"} 
+          onChange={handleChangeValue} 
+        />
+      </div>
+      {
+        !inputChecked ? "" : 
+          <h1>Selected {inputChecked === "Linkedin" 
+            ? <BsLinkedin /> 
+            : <BsInstagram />} 
+          </h1>
+      }  
     </div>
   );
 }
